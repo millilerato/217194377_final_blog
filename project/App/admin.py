dd161e1dae151ba776dd8a8c9dbb29754e136187
millilerato <112854674+millilerato@users.noreply.github.com>
2022-11-18 18:47:27 +0200

    Add files via upload

---
+++ project/App/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Post
+# Register your models here.
+
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display =('title', 'Slug', 'status', 'created_on')
+    list_filter =('status',)
+    search_fields =['title', 'content']
+
+
+
+
+admin.site.register(Post, PostAdmin)
